d18c898554f4f606a1906d67c50e4794ce740599
zoew2 <zoew2@washington.edu>
2019-04-09 16:17:23 -0700

    adding file skeleton and method stubs

---
+++ src/content_selector.py
@@ -0,0 +1,14 @@
+
+
+class ContentSelector:
+    """
+    Functions to summarize documents
+    """
+
+    def select_content(self, input_data):
+        """
+        Select the salient content for the summary
+        :return:
+        """
+
+        return input_data
