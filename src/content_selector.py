45e5121dd1bf5195077423a9526662c697ad977a
genevievepeaslee <genevievepeaslee@gmail.com>
2019-04-14 21:37:46 -0700

    first implemention of lead sentence summary generator

--- src/content_selector.py
+++ src/content_selector.py
@@ -5,10 +5,14 @@ class ContentSelector:
     Functions to summarize documents
     """
 
-    def select_content(self, input_data):
+    def select_content(self, documents):
         """
         Select the salient content for the summary
-        :return:
+        (lead sentence of each document, ordered by date - least to most recent)
+        :param: list of Document objects
+        :return: list of Sentence objects
         """
+        chron_docs = sorted(documents, key=Document.date)  # most recent last
+        selected_content = [doc.get_sen_bypos(0) for doc in chron_docs]
 
-        return input_data
+        return selected_content
--- src/summary_generator.py
+++ src/summary_generator.py
@@ -1,17 +1,24 @@
 from .content_selector import ContentSelector
-
+from .class_document import Document
 
 class SummaryGenerator:
     """
     Functions to summarize documents
+    Current implementation produces a summary consisting of the first sentence of each input document ordered by date
+    (reverse chron)
     """
 
     def __init__(self, documents):
         """
         Initialize this class by saving input documents
-        :param documents: the input documents
+        :param documents: list of Document objects
         """
-        self.documents = self.pre_process(documents)
+
+        self.documents = documents
+
+        # self.documents = self.pre_process(documents) I think this will need to change but not sure how until we
+        # figure out what preprocessing we're actually doing and how we'll store the processed text separate from the
+        # original text
 
     def pre_process(self, documents):
         """
@@ -24,7 +31,7 @@ class SummaryGenerator:
     def select_content(self):
         """
         Select the salient content for the summary
-        :return:
+        :return: list of Sentence objects
         """
 
         selector = ContentSelector()
@@ -42,11 +49,22 @@ class SummaryGenerator:
     def realize_content(self, ordered_info):
         """
         Determine the surface realization for the content
-        :param ordered_info:
+        :param ordered_info: list of Sentence objects
         :return:
         """
 
-        return ordered_info
+        output_content = []
+        token_total = 0
+        while ordered_info:
+            next_sent = ordered_info.pop().curr_sentence
+            next_sent_len = len(next_sent.split(' '))
+            if token_total + next_sent_len < 100:
+                output.append(next_sent)
+                token_total += next_sent_len
+            else:
+                break
+        output = '\n'.join(output_content)  # one sentence per line
+        return output
 
     def generate_summary(self):
         """
