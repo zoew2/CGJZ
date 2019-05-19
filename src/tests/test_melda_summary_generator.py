7d1d367a67260348550c69a4a5849ea0280148c7
Julia McAnallen <jmcanal@uw.edu>
2019-05-19 12:46:38 -0700

    Revert "Revert "Revert "small test changes"""
    This reverts commit f47ed99881f8f2cc890c5b124855e3c099b5f968.

--- src/tests/test_melda_summary_generator.py
+++ src/tests/test_melda_summary_generator.py
@@ -66,6 +66,5 @@ class MeldaSummaryGeneratorTests(unittest.TestCase):
             summary = summarizer.generate_summary(self.idf)
             self.assertIsNot(summary, None)
 
-
 if __name__ == '__main__':
     unittest.main()
