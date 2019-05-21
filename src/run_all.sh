2e781ad0c3ed9d3125960f64759695bfdf2c417f
Zoe Winkworth <43589990+zoew2@users.noreply.github.com>
2019-05-20 20:56:36 -0700

    Update run_all.sh

--- src/run_all.sh
+++ src/run_all.sh
@@ -28,4 +28,4 @@ RESULTS_FILE_PATH='../results'
 # run melda
 time /opt/python-3.6/bin/python3.6 run_summarization.py "$DATA_DIR"/GuidedSumm10_test_topics.xml melda
 # evaluate melda
-"$ROUGE_DIR"/ROUGE-1.5.pl -e "$ROUGE_DIR"/data -a -n 2 -x -m -c 95 -r 1000 -f A -p 0.5 -t 0 -l 100 -s -d "$RESULTS_FILE_PATH"/melda_rouge_run.xml &> "$RESULTS_FILE_PATH"/D3_rouge_scores.out
+"$ROUGE_DIR"/ROUGE-1.5.5.pl -e "$ROUGE_DIR"/data -a -n 2 -x -m -c 95 -r 1000 -f A -p 0.5 -t 0 -l 100 -s -d "$RESULTS_FILE_PATH"/melda_rouge_run.xml &> "$RESULTS_FILE_PATH"/D3_rouge_scores.out
