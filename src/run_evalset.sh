849f70e84fb27012b7102fe6a435953a833f18b8
genevievepeaslee <genevievepeaslee@gmail.com>
2019-06-01 13:31:17 -0700

    fixes

--- src/run_evalset.sh
+++ src/run_evalset.sh
@@ -10,7 +10,7 @@ RESULTS_FILE_PATH='../results'
 #"$ROUGE_DIR"/ROUGE-1.5.5.pl -e "$ROUGE_DIR"/data -a -n 2 -x -m -c 95 -r 1000 -f A -p 0.5 -t 0 -l 100 -s -d "$RESULTS_FILE_PATH"/lead_rouge_evalset.xml &> "$RESULTS_FILE_PATH"/lead_rouge_evalset_scores.out
 
 # run mead
-time /opt/python-3.6/bin/python3 run_summarization.py "$DATA_DIR"/GuidedSumm11_test_topics.xml mead --output_dir ../outputs/D4_evaltest/
+time /opt/python-3.6/bin/python3 run_summarization.py "$DATA_DIR"/GuidedSumm11_test_topics.xml mead --output_dir ../outputs/D4_eval/
 # evaluate mead
 "$ROUGE_DIR"/ROUGE-1.5.5.pl -e "$ROUGE_DIR"/data -a -n 2 -x -m -c 95 -r 1000 -f A -p 0.5 -t 0 -l 100 -s -d "$RESULTS_FILE_PATH"/mead_rouge_evalset.xml &> "$RESULTS_FILE_PATH"/mead_rouge_evalset_scores.out
 
