09d324cc966e8dc8c314e1f0b381c8b17123057a
Zoe Winkworth <zoew2@washington.edu>
2019-06-01 18:28:07 -0700

    updating rules

--- src/melda/melda_summary_generator.py
+++ src/melda/melda_summary_generator.py
@@ -42,6 +42,118 @@ class MeldaSummaryGenerator(MeadSummaryGenerator):
             MeldaInfoOrdering(self.args, self.content_selector.selected_content
                               ).run_cohesion_gradient(self.documents)
 
+    def compress_sentences(self):
+        sentences = []
+        for sentence in self.content_selector.selected_content:
+            compressed = []
+            ignore = []
+            remove_punct = remove_that = False
+            for token in sentence.processed:
+                pos = token.pos_
+                dep = token.dep_
+                head = token.head.text
+                head_dep = token.head.dep_
+                head_subtree = [t for t in token.head.subtree]
+                subtree = [t for t in token.subtree]
+                attributives = ['said', 'claimed', 'reported']
+                attributive_ancestors = [a for a in token.ancestors if a.text in attributives]
+                attributive_children = [c for a in attributive_ancestors for c in a.children if c.dep_ is 'nsubj']
+                attribution_subj = [t.text for c in attributive_children for t in c.subtree]
+                children = [child for child in token.children]
+                cchildren = [g for c in children for g in c.children]
+                child_deps = [child.dep_ for child in children]
+
+
+                if token.i in ignore or not token.text.rstrip():
+                    continue
+
+                # remove all adverbs
+                if token.dep_ is 'advmod':
+                    print("removing adverb: " + token.text)
+                    remove_punct = True
+                    continue
+
+                # remove initial conj
+                if token.pos_ is 'CCONJ' and token.i is 0:
+                    print("removing initial conj: " + token.text)
+                    remove_punct = True
+                    continue
+
+                # remove punctuation following removed adv or initial conj unless it's sentence final
+                if remove_punct:
+                    if token.is_punct and token.i < len(token.doc)-1:
+                        continue
+                    else:
+                        remove_punct = False
+
+                # remove attributive words
+                if token.text in attributives:
+                    prep_indicies = [t.i for pp in token.children for t in pp.subtree if pp.dep_ is 'prep']
+                    ignore.extend(prep_indicies)
+                    pp = [t.text for pp in token.children for t in pp.subtree if pp.dep_ is 'prep']
+                    print("removing attribution and preps: " + token.text + " " + " ".join(pp))
+                    remove_that = True
+                    continue
+
+                # remove 'that' if it's the start of a relative clause after an attributive word
+                if remove_that:
+                    if token.dep_ is 'mark':
+                        continue
+                    else:
+                        remove_that = False
+
+                # remove appositives and parenthesized info
+                if len(child_deps) > 2 and 'appos' in child_deps:
+                    appos = [t.text for c in children for t in c.subtree if c.dep_ == 'appos']
+                    appos_indicies = [t.i for c in children for t in c.subtree if c.dep_ == 'appos']
+                    ignore.extend(appos_indicies)
+                    remove_punct = True
+                    print("removing appositive: " + " ".join(appos))
+
+                # remove the subject of an attribution
+                if attribution_subj and token.text == attribution_subj[0]:
+                    tree_indices = [t.i for c in attributive_children for t in c.subtree]
+                    ignore.extend(tree_indices)
+                    attrib_phrase = [t.text for c in attributive_children for t in c.subtree]
+                    print("removing attributor: " + " ".join(attrib_phrase))
+                    continue
+
+                # remove temporal modifiers
+                pp = [t for t in token.subtree]
+                descendants = [c for c in token.children]
+                descendants.extend([g for c in token.children for g in c.children])
+                deps = [c.dep_ for c in descendants]
+                pp_ent_types = [t.ent_type_ for t in pp]
+                temporal_types = ['TIME', 'DATE']
+                has_temporal = set(pp_ent_types) & set(temporal_types) or 'nummod' in deps
+                if token.dep_ == 'prep' and has_temporal:
+                    pp_indicies = [t.i for t in pp]
+                    ignore.extend(pp_indicies)
+                    print("removing temp mod: " + " ".join([t.text for t in pp]))
+                    continue
+
+                if not compressed and token.is_punct:
+                    continue
+
+                compressed.append(token.text)
+
+            # fix capitalization, detokenize and strip any weird beginnings
+            if not compressed:
+                continue
+            compressed[0] = compressed[0].capitalize()
+            new_sentence = TreebankWordDetokenizer().detokenize(compressed)
+            sentence_text = self.strip_beginning(new_sentence)
+
+            # set compressed sentence on sentence object
+            sentence.compressed = sentence_text
+            sentences.append(sentence)
+
+        self.content_selector.selected_content = sentences
+
+    def realize_content(self):
+        self.compress_sentences()
+        return super(MeldaSummaryGenerator, self).realize_content()
+
     def generate_summary(self, idf_array=None):
         """
         Generate the summary
---
+++ src/tests/test_melda_sentence_compression.py
@@ -0,0 +1,127 @@
+import unittest
+from src.helpers.class_preprocessor import Preprocessor
+from src.melda.melda_content_selector import MeldaContentSelector
+from src.helpers.class_sentence import Sentence
+from src.run_summarization import parse_args
+from src.melda.melda_summary_generator import MeldaSummaryGenerator
+
+class MeldaSentenceCompressionTests(unittest.TestCase):
+    """
+    Tests for MeldaInfoOrdering
+    """
+    Preprocessor.load_models()
+
+    s0 = Sentence("In a park somewhere, a bunch of puppies played fetch with their owners today.", 1)
+    s1 = Sentence("I took my small puppy to the dog park today.", 1)
+    s2 = Sentence("He loves playing so he liked to run around with the other dogs playing fetch.", 1)
+    s3 = Sentence("Puppies love playing fetch.", 1)
+
+    input_summary = [s0, s1, s2, s3]
+
+    args = parse_args(['test_data/test_topics.xml', 'test'])
+    args.n = 1
+
+    selector = MeldaContentSelector()
+    generator = MeldaSummaryGenerator([], selector, args)
+
+    def test_remove_adverbs(self):
+        s = Sentence("Puppies love running quickly and playing loudly.", 1)
+        self.selector.selected_content = [s]
+        self.generator.compress_sentences()
+        summary = "\n".join([s.compressed for s in self.selector.selected_content])
+
+        expected = "Puppies love running and playing."
+        self.assertEqual(summary, expected)
+
+    def test_remove_initial_conj(self):
+        s = Sentence("But, puppies are great.", 1)
+        self.selector.selected_content = [s]
+        self.generator.compress_sentences()
+        summary = "\n".join([s.compressed for s in self.selector.selected_content])
+
+        expected = "Puppies are great."
+        self.assertEqual(summary, expected)
+
+    def test_remove_parens(self):
+        s = Sentence("The puppy (aka Mr. Mayor) was the cutest.", 1)
+        self.selector.selected_content = [s]
+        self.generator.compress_sentences()
+        summary = "\n".join([s.compressed for s in self.selector.selected_content])
+
+        expected = "The puppy was the cutest."
+        self.assertEqual(summary, expected)
+
+    def test_remove_appositives(self):
+        s = Sentence("Dennis, the cutest puppy in the park, ran towards the ball.", 1)
+        self.selector.selected_content = [s]
+        self.generator.compress_sentences()
+        summary = "\n".join([s.compressed for s in self.selector.selected_content])
+
+        expected = "Dennis ran towards the ball."
+        self.assertEqual(expected, summary)
+
+    def test_remove_junk(self):
+        s = Sentence("Seattle, WA --- Puppies are great.", 1)
+        self.selector.selected_content = [s]
+        self.generator.compress_sentences()
+        summary = "\n".join([s.compressed for s in self.selector.selected_content])
+
+        expected = "Puppies are great."
+        self.assertEqual(summary, expected)
+
+    def test_remove_attributions(self):
+        s = Sentence("Julia said that puppies are cute.", 1)
+
+        self.selector.selected_content = [s]
+        self.generator.compress_sentences()
+        summary = "\n".join([s.compressed for s in self.selector.selected_content])
+
+        expected = "Puppies are cute."
+        self.assertEqual(summary, expected)
+
+    def test_remove_attribution_phrases(self):
+        s = Sentence("Seattle State Bureau of Animal Rating said "
+                     "in a press release that puppies are cute.", 1)
+
+        self.selector.selected_content = [s]
+        self.generator.compress_sentences()
+        summary = "\n".join([s.compressed for s in self.selector.selected_content])
+
+        expected = "Puppies are cute."
+        self.assertEqual(summary, expected)
+
+    def test_remove_temporal_mod(self):
+        s = Sentence("By 8 a.m. on Saturday the park was full of puppies.", 1)
+
+        self.selector.selected_content = [s]
+        self.generator.compress_sentences()
+        summary = "\n".join([s.compressed for s in self.selector.selected_content])
+
+        expected = "The park was full of puppies."
+        self.assertEqual(summary, expected)
+
+    def test_remove_mod_rel(self):
+        s = Sentence("Joe said that by 8 a.m. on Saturday the park was full of puppies.", 1)
+
+        self.selector.selected_content = [s]
+        self.generator.compress_sentences()
+        summary = "\n".join([s.compressed for s in self.selector.selected_content])
+
+        expected = "The park was full of puppies."
+        self.assertEqual(summary, expected)
+
+    def test_bad(self):
+        s = Sentence("Heilongjiang Provincial Bureau of Environmental Protection said in a press release that by 6 a.m. on Saturday, concentration of nitrobenzene monitored at Sujiatun upstream Sifangtai, one major water intake spot of Harbin, capital of northeast China's Heilongjiang Province, fell to 0.0793 mg per liter, but above the state safety standard of 0.017 mg per liter, but the density of benzene stood at 0.0011 mg per liter, which is within   the state safety benchmark.", 1)
+
+        self.selector.selected_content = [s]
+        self.generator.compress_sentences()
+        summary = "\n".join([s.compressed for s in self.selector.selected_content])
+
+        expected = "Concentration of nitrobenzene monitored at Sujiatun upstream Sifangtai fell, " \
+                   "but above the state safety standard, but the density of benzene stood, " \
+                   "which is within the state safety benchmark."
+        self.assertEqual(summary, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
