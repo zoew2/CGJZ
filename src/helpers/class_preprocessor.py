7d2e183899ed83860b9080b732354b2325833edc
youyunzh <youyunzh@usc.edu>
2019-06-02 20:07:00 -0700

    prestrip

--- src/helpers/class_preprocessor.py
+++ src/helpers/class_preprocessor.py
@@ -4,6 +4,7 @@ from nltk.corpus import stopwords
 import re
 import itertools
 import nltk
+import numpy as np
 
 class Preprocessor:
     """
@@ -25,26 +26,68 @@ class Preprocessor:
         return Preprocessor.spacynlp(raw_sentence)
 
     @staticmethod
-    def get_processed_tokens(processed):
+    def get_processed_tokens(sen):
         # entities = [e.text.lower().split() for e in processed.ents]
         # entities = list(itertools.chain.from_iterable(entities))
         # processed = nltk.tokenize.word_tokenize(processed)
 
-        processed_tokens = []
-        # processed_tokens.extend([e.text for e in processed.ents])
-        all_entities = True
-        for w in processed:
-            # w = w.lemma_.lower()
-            w = w.text.lower()
-            # w = w.lower()
-            if w == '-pron-' or not w.rstrip():
-                continue
+        # processed_tokens = []
+        # # processed_tokens.extend([e.text for e in processed.ents])
+        # all_entities = True
+        # for w in processed:
+        #     # w = w.lemma_.lower()
+        #     w = w.text.lower()
+        #     # w = w.lower()
+        #     if w == '-pron-' or not w.rstrip():
+        #         continue
+        #
+        #     if w not in string.punctuation and w not in Preprocessor.stop_words: # and w not in entities:
+        #         all_entities = False
+        #         processed_tokens.append(w)
+        #
+        # return [] if all_entities else processed_tokens
+        entity_ind = [0] * len(sen)
+        ind = 1
+        for ent in sen.ents:
+            for i in range(ent.start, ent.end):
+                entity_ind[i] = ind
+            ind += 1
 
-            if w not in string.punctuation and w not in Preprocessor.stop_words: # and w not in entities:
-                all_entities = False
-                processed_tokens.append(w)
+        # [1, 1, 0, 0, 0, 2, 0, 0, 3, 3, 3] # index > 0 are NEs
 
-        return [] if all_entities else processed_tokens
+        if np.prod(entity_ind) != 0:  # if every word is a NE, not a sentence.
+            return []
+
+        # linking NE
+        new_toks = []
+        ent_ind = 0  # pointer to entities
+        for i in range(len(entity_ind)):
+
+            if_ent = entity_ind[i]
+            if if_ent > 0:  # if token is in an entity, just add to the new_toks, if not add stemmed word
+                if i == 0:
+                    new_toks.append(sen.ents[0].text)
+                    ent_ind += 1
+                elif entity_ind[i - 1] == 0:  # the tok before is not in a NE
+                    new_toks.append(sen.ents[ent_ind].text)
+                    ent_ind += 1
+                elif entity_ind[i - 1] < if_ent:  # another NE follow right after it
+                    new_toks.append(sen.ents[ent_ind].text)
+                    ent_ind += 1
+            else:
+
+                w = sen[i].lemma_
+                if w != '-PRON-':  # if w is not a NE, lowercase it
+                    w = w.lower()
+                else:
+                    # w = sen[i].text
+                    continue
+                if w not in string.punctuation and w not in Preprocessor.stop_words:  # Strip punctuation and stopwords from sentence tokens
+
+                    new_toks.append(w)
+
+        return new_toks
+
 
     @staticmethod
     def is_bad_sentence(raw_sentence):
@@ -58,7 +101,7 @@ class Preprocessor:
 
     @staticmethod
     def strip_beginning(raw_sentence):
-        matches = re.finditer(r"^[A-Z].*(-{2}|_)", raw_sentence)
+        matches = re.finditer(r"^[*A-Z].*(-{2}|_)", raw_sentence)
         indicies = [m.end() for m in matches]
         new_start_idx = indicies[0] if indicies else -1
         return raw_sentence[new_start_idx+1:]
--- src/tests/test_preprocessor.py
+++ src/tests/test_preprocessor.py
@@ -13,15 +13,16 @@ class PreprocessorTests(unittest.TestCase):
     def test_sent_preprocessing(self):
 
         raw_sentence = "He took his small puppy to New York today ."
+
         processed = Preprocessor.get_processed_sentence(raw_sentence)
         expected_tokenized_sen= ['take', 'small', 'puppy', 'New York', 'today']
 
         tokenized_sen=Preprocessor.get_processed_tokens(processed)
         self.assertCountEqual(expected_tokenized_sen, tokenized_sen)
 
-        raw_sentence1 = "In the morning he took his small puppy to New York today ."
+        raw_sentence1 = "In the morning he took his new puppy to New York today ."
         processed1 = Preprocessor.get_processed_sentence(raw_sentence1)
-        expected_tokenized_sen1 = ['the morning', 'take', 'small', 'puppy', 'New York', 'today']
+        expected_tokenized_sen1 = ['the morning', 'take', 'new', 'puppy', 'New York', 'today']
 
         tokenized_sen1=Preprocessor.get_processed_tokens(processed1)
         self.assertCountEqual(expected_tokenized_sen1, tokenized_sen1)
@@ -33,13 +34,22 @@ class PreprocessorTests(unittest.TestCase):
         tokenized_sen2=Preprocessor.get_processed_tokens(processed2)
         self.assertCountEqual(expected_tokenized_sen2, tokenized_sen2)
 
-        raw_sentence3 = "Washington is New York ."
-        expected_tokenized_sen3 = ['Washington', 'New York']
-
-        tokenized_sen3=Preprocessor.sent_preprocessing(raw_sentence3)
-        self.assertEqual(expected_tokenized_sen3, tokenized_sen3)
 
 
+
+
+    def test_strip_beginning(self):
+
+
+        raw_sent1 = "*Friday, Feb. 26, 1999 Iranians Vote in Local Elections TEHRAN, Iran (AP) -- Iranians cast ballots today in the nation’s first election of local officials in 20 years."
+        result = Preprocessor.strip_beginning(raw_sent1)
+        self.assertEqual(result, "Iranians cast ballots today in the nation’s first election of local officials in 20 years.")
+
+
+        raw_sent2 = "JAKARTA, November 19 (Xinhua) -- Local people and students in Indonesia’s capital Jakarta held an anti-Soeharto mass demonstration, starting at 14:00 local time on Thursday."
+        result = Preprocessor.strip_beginning(raw_sent2)
+        self.assertEqual(result,
+                         "Local people and students in Indonesia’s capital Jakarta held an anti-Soeharto mass demonstration, starting at 14:00 local time on Thursday.")
 
 
 if __name__ == '__main__':
