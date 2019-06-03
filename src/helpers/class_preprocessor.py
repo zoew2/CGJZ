77b570d6ba5b297217aa72ba046df184c55bc3d1
genevievepeaslee <genevievepeaslee@gmail.com>
2019-06-02 18:48:23 -0700

    new NER approach

--- src/helpers/class_preprocessor.py
+++ src/helpers/class_preprocessor.py
@@ -1,6 +1,6 @@
 import spacy
 import string
-from nltk.corpus import stopwords
+    from nltk.corpus import stopwords
 import re
 import itertools
 import nltk
@@ -38,14 +38,23 @@ class Preprocessor:
         :return: List of Strings (empty if all tokens are punctuation, stopwords or, optionally, NEs)
         commented code is for lemmatizing non-NEs
         """
+
         # entities = [e.text.lower().split() for e in processed.ents]
         # entities = list(itertools.chain.from_iterable(entities))
         # processed = nltk.tokenize.word_tokenize(processed)
 
+        entities = set(processed.ents)
+        entity_tokens = set()
+        for ent in entities:
+            for token in ent:
+                entity_tokens.add(token)
+        unprocessed = set(processed) - entity_tokens
+
         processed_tokens = []
         # processed_tokens.extend([e.text for e in processed.ents])
+        processed_tokens.extend(e.text for e in entities)
         all_entities = True
-        for w in processed:
+        for w in unprocessed:
             w = w.text.lower()
             # if w.text.lower() in entities:
             #     continue
@@ -55,7 +64,7 @@ class Preprocessor:
             if w == '-pron-' or not w.rstrip():
                 continue
 
-            if w not in string.punctuation and w not in Preprocessor.stop_words: #and w not in entities:
+            if w not in string.punctuation:  # and w not in Preprocessor.stop_words: and w not in entities:
                 all_entities = False
                 processed_tokens.append(w)
 
